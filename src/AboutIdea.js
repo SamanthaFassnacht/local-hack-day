202651f0287433392b750383bfcbbf40298fde94
SamanthaFassnacht <samantha.fassnacht@gmail.com>
2018-12-01 12:52:37 -0500

    Add blank about pages

---
+++ src/AboutIdea.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class AboutIdea extends Component {
+    render() {
+        return (
+            //TODO: Add html code here!!
+            <div></div>
+        );
+    }
+}
+
+export default AboutIdea;
--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Nav from './Nav';
-import AboutIdea from './AboutIdea';
-import AboutUs from './AboutUs';
+import MainContent from './MainContent';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Nav></Nav>
+        <MainContent></MainContent>
       </div>
     );
   }
