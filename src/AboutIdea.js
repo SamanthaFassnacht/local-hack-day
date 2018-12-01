ef3572b30ef141c93b3707ce27ae84114c5eab5c
Enoch Wong <32200626+enwong10@users.noreply.github.com>
2018-12-01 15:12:30 -0500

    Update About Us

--- src/AboutIdea.js
+++ src/AboutIdea.js
@@ -7,11 +7,11 @@ class AboutIdea extends Component {
             <div>
                 <h1 class="header" id="about_us_header"> Meet the Team </h1>
                 <p class="body_text" id="about_us_body"> We are a group of diverse university students working together to investigate the mysteries of our galaxy one project at a time. 
-\nMichael is currently a second-year undergraduate student at UTSG majoring in physics and astronomy. You can always find him at either the library or break dancing on the dance floor. 
-\nSamantha is a part of the CS and Tech Leadership Initiative program at UTSG. Don’t mess with her – she trains in naginata (Japanese sword-fighting)
-\nRichard is a second-year physics major at UTSG. He likes rock-climbing and the challenge it brings. 
-\nKshitij studies CS undergrad at UTSG. He spends his spare time watching on Netflix and Youtube. 
-\nEnoch attends the University of Waterloo for Systems Design Engineering. Sleep. </p>
+Michael is currently a second-year undergraduate student at UTSG majoring in physics and astronomy. You can always find him at either the library or break dancing on the dance floor. 
+Samantha is a part of the CS and Tech Leadership Initiative program at UTSG. Don’t mess with her – she trains in naginata (Japanese sword-fighting)
+Richard is a second-year physics major at UTSG. He likes rock-climbing and the challenge it brings. 
+Kshitij studies CS undergrad at UTSG. He spends his spare time watching on Netflix and Youtube. 
+Enoch attends the University of Waterloo for Systems Design Engineering. Sleep. </p>
             
             </div>
         );
