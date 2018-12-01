73ec4262a92b83a533720ca7428a8620ebdbb80c
Kshitij Shah <kshnik@gmail.com>
2018-12-01 14:58:14 -0500

    Made database work

--- backend/FlaskProj.py
+++ backend/FlaskProj.py
@@ -1,42 +1,50 @@
-import mysql.connector
+from typing import List, Tuple
+
+import pyodbc
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-class DatabaseConnection:
-    mydb = mysql.connector.connector.connect(
-        host="http://spacestourssim.database.windows.net",
-        user="USER",
-        passwd="UTSChack2018",
-        database="SPACE_TOURS_SIM_MAIN"
-    )
 
-    def getAllPlanetsInit(self):
-        cur = mydb.cursor()
-        cur.execute("SELECT * FROM Planets")
-        result = cur.fetchall()
+server = "tcp:spacetourssim.database.windows.net"
+username = "USER@spacetourssim"
+password = "UTSChack2018"
+driver = "{ODBC Driver 13 for SQL Server}"
+database = "SPACE_TOURS_SIM_MAIN"
 
-        return result
+connection = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+password)
+cur = connection.cursor()
 
-    def getAllRocketNames(self):
-        cur = mydb.cursor()
-        cur.execute("SELECT MoTName from ModeofTransportation")
-        result = cur.fetchall()
-
-        return result
-
-    def getRocketInfoFromName(self, name):
-        cur = mydb.cursor()
-        cur.execute("SELECT * FROM ModeofTransportation WHERE MoTName = %s", (name))
-        result = cur.fetchone()
+print("Connection established")
 
 
-class PlanetsInit:
-    def __init__(self):
-        pass
+def getAllPlanetsInit() -> List[Tuple[str]]:
+    cur.execute("SELECT * FROM Planets")
+    result = cur.fetchall()
 
-class GetTrajectorySet:
-    def __init__(self):
-        pass
+    return result
+
+
+def getAllRocketNames() -> List[Tuple[str]]:
+    cur.execute("SELECT MoTName from ModeofTransportation")
+    results = cur.fetchall()
+
+    return [name[0] for name in results]
+
+
+def getRocketInfoFromName(name: str) -> Tuple[str]:
+    cur.execute("SELECT * FROM ModeofTransportation WHERE MoTName = '" + (name) + "'")
+    result = cur.fetchone()
+
+    return result
+
+
+class 
+
+#
+# if __name__ == "__main__":
+#     print(getAllPlanetsInit())
+#     print(getAllRocketNames())
+#     print(getRocketInfoFromName("Rocket"))
